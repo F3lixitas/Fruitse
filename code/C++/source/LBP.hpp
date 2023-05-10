88ac5810e986463c2ed412828a5e4f8bec27da71
Felix <felixriedel25@gmail.com>
2023-05-10 09:04:24 +0200

    Auto stash before merge of "main" and "origin/main"

---
+++ code/C++/source/LBP.hpp
@@ -0,0 +1,9 @@
+#ifndef FRUITSE_LBP_HPP
+#define FRUITSE_LBP_HPP
+
+#include "opencv2/opencv.hpp"
+
+template <typename T>
+cv::Mat LBPImage(const cv::Mat& image);
+
+#endif //FRUITSE_LBP_HPP
--- code/C++/source/main.cpp
+++ code/C++/source/main.cpp
@@ -1,17 +1,29 @@
-#include "opencv2/opencv.hpp"
+#include "LBP.hpp"
+#include <filesystem>
+#include <iostream>
 
-/**
+#define PROJECT_ROOT_PATH std::filesystem::current_path().parent_path().parent_path().parent_path()
+
+/*
  * Charge une image ou une série d'images et écrit ses caractéristiques dans un fichier csv.
  */
-
 int main(int argc, char* argv[]) {
-    cv::Mat image;
-    cv::VideoCapture* webcam = new cv::VideoCapture(0);
-    cv::namedWindow("WebCam",1);
-    if(webcam->read(image)) {
-        cv::imshow("WebCam", image);
+    if(argc >=2){
+        for(int i = 1; i < argc; i++) {
+            std::string s = argv[i];
+            std::filesystem::path path = std::filesystem::absolute(PROJECT_ROOT_PATH.concat(s));
+            if(std::filesystem::exists(path)) {
+                cv::Mat image = cv::imread(std::filesystem::absolute(PROJECT_ROOT_PATH.concat(s)).string());
+                cv::Mat dst;
+                cv::cvtColor(image, dst, cv::COLOR_BGR2GRAY);
+                cv::Mat lbp = LBPImage<unsigned char>(dst);
+                cv::namedWindow("Image", 1);
+                cv::imshow("Image", lbp);
+                cv::waitKey(0);
+            } else {
+                std::cerr << "The path <" << path.string() << "> does not exist !\n";
+            }
+        }
     }
-    cv::waitKey(0);
-    delete webcam;
     return 0;
 }
